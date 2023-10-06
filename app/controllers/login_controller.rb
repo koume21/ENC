dfbb9c7592d000e74ef245c7f8d1961294d53673
kuroda <kuroda@example.com>
2023-10-06 14:48:19 +0900

    hennkou

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,18 @@
 class LoginController < ApplicationController
-  def login
+  def new
+    @users = User.new
+  end
+  def index
     @users = User.all
+  end
+
+  def create
+    @users = User.new(user_params)
+    @users.save
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_id, :password)
   end
 end
