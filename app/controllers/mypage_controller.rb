4eca02269735b68b9fb9849c672b519539a31311
Koumei <t.koumei.0121@gmail.com>
2023-10-10 11:42:56 +0900

    プロフィール変更

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,15 @@
 class MypageController < ApplicationController
   def profile
-    @user = User.where("id = 1")
-    @user_profile = Profile.where("user_id = 1")
+    @user = User.find(1)
+    @user.update(profile_params)
+  end
+
+  def update
+    @profile = Profile.find_by(user_id: 1)
+
+  end
+  private
+  def profile_params
+    params.require(:profile).permit(:user_name, :school, :profile_image, :grade, :club, :type, :comments)
   end
 end
