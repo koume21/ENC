af84b6b373fecd3872e8eb6928ee36fea65329d4
shuto <zhongrengaomu09@gamil.com>
2023-10-11 11:29:37 +0900

    修正

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
 
   def create
     user = User.find_by(login: params[:user_id])
-    if user && user.passward == params[:password]  then
+    if user && user.password == params[:password]  then
       redirect_to '/mypage/profile'
     else
       flash.now[:danger] = 'メールアドレスかパスワードが間違っています。'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { login: @user.login, passward: @user.passward } }
+      post users_url, params: { user: { login: @user.login, password: @user.password } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { login: @user.login, passward: @user.passward } }
+    patch user_url(@user), params: { user: { login: @user.login, password: @user.password } }
     assert_redirected_to user_url(@user)
   end
 
