be4917da1454e6970abf8803b0f7a495d36714eb
shuto <zhongrengaomu09@gamil.com>
2023-10-13 15:14:12 +0900

    syusei

--- app/controllers/new_account_controller.rb
+++ app/controllers/new_account_controller.rb
@@ -2,18 +2,12 @@ class NewAccountController < ApplicationController
   def new
   end
 
-  def create
-    @user = User.new(params[:user_id],params[:password])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to '/mypage/profile', notice: "ユーザー登録完了" }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+  def create    
+    @user = User.new(login: params[:user_id],password: params[:password])
+    if @user.save
+      redirect_to '/mypage/profile'
+    else
+      render :new
     end
   end
-
 end
