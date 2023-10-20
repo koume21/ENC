2f4fb3babe633abd7f7a2438d7e9da8fc63cd27b
Koumei <t.koumei.0121@gmail.com>
2023-10-20 15:04:26 +0900

    mypage

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -5,16 +5,16 @@ class MypageController < ApplicationController
   end
 
   def update
-    p "aaa"
-    # profile = Profile.find_by(user_id: 1)
-    # if profile.update(profile_params)
-    #   redirect_to '/'
-    # else
-    #   redirect_to '/mypage/profile'
-    # end
+    profile = Profile.find_by(user_id: 1)
+
+    if profile.update(profile_params)
+      redirect_to mypage_profile_path,notice:"Update successed"
+    else
+      redirect_to mypage_profile_path,notice:"Update loss"
+    end
   end
   private
   def profile_params
-    params.require(:profile).permit(:user_name, :school, :profile_image, :grade, :club, :type, :comments)
+    params.require(:profile).permit(:user_name, :school_id, :profile_image, :grade, :club_id, :type_id, :comments)
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -36,16 +36,15 @@ class ProfilesController < ApplicationController
 
   # PATCH/PUT /profiles/1 or /profiles/1.json
   def update
-    p "bbb"
-    # respond_to do |format|
-    #   if @profile.update(profile_params)
-    #     format.html { redirect_to profile_url(@profile), notice: "Profile was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @profile }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @profile.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @profile.update(profile_params)
+        format.html { redirect_to profile_url(@profile), notice: "Profile was successfully updated." }
+        format.json { render :show, status: :ok, location: @profile }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @profile.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /profiles/1 or /profiles/1.json
