6607bc3081dc6380dab2c83b1b6f5864f212e60f
kuroda <kuroda@example.com>
2023-11-07 11:06:36 +0900

    変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    @good_ranks = User.find(Good.group(:good_id).order('count(good_id) desc').limit(3).pluck(:good_id)
+    @good_ranks = User.find(Good.group(:good_id).order('count(good_id) desc').limit(3).pluck(:good_id))
     @schools = School.all
     @clubs = Club.all
     @types = Type.all
