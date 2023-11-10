ee3950e23a07cca2615ea71ac3fd40c2ef4308c4
shuto <zhongrengaomu09@gamil.com>
2023-11-10 14:45:37 +0900

    変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,20 @@ class SearchController < ApplicationController
     @clubs = Club.all
     @types = Type.all
   end
+  
   def search
-    
+    @good_ranks = User.find(Good.group(:good_id).order('count(good_id) desc').limit(3).pluck(:good_id))
+    @schools = School.all
+    @clubs = Club.all
+    @types = Type.all
+    @school = params[:school][:id]
+
+    p "***************************************"
+    p params[:school][:id]
+    p @school
+    p "***************************************"
+
+    # @search = School.where(id: @school)
+    render :index
   end
 end
