3749fb0d203983e606073d3f775d515bebef2adc
Koumei <t.koumei.0121@gmail.com>
2023-11-17 16:12:49 +0900

    挫折

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,14 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-    def connect
-      self.current_user = find_verified_user
-    end
-    private
-    def find_verified_user
-      verified_user = User.find_by(id: cookies.encrypted[:login_id])
-      return reject_unauthorized_connection unless verified_user
-      verified_user
-    end
   end
 end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,10 +12,20 @@ const appRoom = consumer.subscriptions.create("RoomChannel", {
   received(data) {
     const message = document.getElementById('message');
     message.insertAdjacentHTML('beforeend', data['message'])
+    current_user_id = $("#current_user_id").val();
+    room_id = $("#room_id").val();
+    console.log(current_user_id);
+    console.log(room_id);
   },
 
   speak: function(message) {
-    return this.perform('speak',{message: message});
+    current_user_id = $("#current_user_id").val();
+    room_id = $("#room_id").val();
+    return this.perform('speak',{
+      message: message,
+      current_user_id: current_user_id,
+      room_id: room_id,
+    });
   }
 });
 window.addEventListener("keydown", function(e) {
