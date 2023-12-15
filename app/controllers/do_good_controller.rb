0b68dcc638f707e71f889e582dcdd75f732f6ace
kuroda <kuroda@example.com>
2023-12-15 15:14:16 +0900

    変更

--- app/controllers/do_good_controller.rb
+++ app/controllers/do_good_controller.rb
@@ -3,13 +3,13 @@ class DoGoodController < ApplicationController
     def create
         @good_do_good = Good.new(good_id: params[:good_id], user_id: session[:login_id])
         @good_do_good.save
-        return
+        redirect_to search_index_path
     end
 
     def destroy
         @good_do_good = Good.find_by(good_id: params[:good_id], user_id: session[:login_id])
         @good_do_good.destroy
-        return
+        redirect_to search_index_path
     end
 
 end
