d87619d886f1c4979b76de269aeb082bf63a5f65
Koumei <t.koumei.0121@gmail.com>
2023-12-20 11:43:20 +0900

    変更

--- app/javascript/packs/mypage.js
+++ app/javascript/packs/mypage.js
@@ -1,23 +1,11 @@
-const buttonOpen = document.getElementById('modal-open');
-const modal = document.getElementById('Modal');
-const buttonClose = document.getElementsByClassName('modalClose')[0];
+'use strict';
+const opener = document.getElementById("open");
+const modal = document.getElementById("modal");
+const closer = document.getElementById("closer");
+opener.addEventListener("click", () => {
+    modal.classList.add("inview");
+}, false);
 
-// ボタンがクリックされた時
-buttonOpen.addEventListener('click', modalOpen);
-function modalOpen() {
-  modal.style.display = 'block';
-}
-
-// バツ印がクリックされた時
-buttonClose.addEventListener('click', modalClose);
-function modalClose() {
-  modal.style.display = 'none';
-}
-
-// モーダルコンテンツ以外がクリックされた時
-addEventListener('click', outsideClose);
-function outsideClose(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
-}
+closer.addEventListener("click", () => {
+    modal.classList.remove("inview");
+}, false);
